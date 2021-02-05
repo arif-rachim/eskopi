2189c8629dee585fcf1d3b16bd7e6acb8862521b
Arif Rachim <marsa.emreef@gmail.com>
2021-02-05 23:57:15 +0400

    Chores : Fix server to support advance post and path action

--- src-service/routes/database.js
+++ src-service/routes/database.js
@@ -3,7 +3,7 @@ import {v4 as uuid} from "uuid"
 
 const router = express.Router();
 
-const entities = {};
+const rootDb = {};
 
 const ACTION_CREATE = 'c';
 const ACTION_UPDATE = 'u';
@@ -11,83 +11,114 @@ const ACTION_DELETE = 'd';
 const DB_PATH = process.env.DB_PATH ?? 'db';
 
 
-const createEntity = (queries) => {
+const createEntity = async (queries) => {
     const id = uuid();
-    entities[id] = queries;
+    const now = new Date();
+    rootDb[id] = {...queries, id_: id, created_: now.toISOString()};
     return id;
 }
-const getEntity = (id) => {
-    if (id in entities) {
-        return entities[id];
+const getEntity = async (id) => {
+    if (id in rootDb) {
+        return rootDb[id];
     }
     return false;
 }
 const getNearestEntity = async (params) => {
     let props = [];
     for (let i = params.length; i > 0; i--) {
-        const param = params[i - 1];
-        const entity = await getEntity(param)
+        const key = params[i - 1];
+        const entity = await getEntity(key)
         if (entity) {
             return [entity, props];
         } else {
-            props.unshift(param);
+            props.unshift(key);
         }
     }
     return [false, props];
 }
 
-router.get('/*', (req, res) => {
-    (async () => {
-        const {params, query} = getParamsAndQuery(req);
-        const {a, ...queries} = query;
-        let result = undefined;
-        if (a === ACTION_CREATE) {
-            let [entity, props] = await getNearestEntity(params);
-            const key = createEntity(queries);
-            if (entity) {
-                if (Array.isArray(entity)) {
-                    entity.push(key);
-                } else {
-                    props = props.join('');
-                    entity[props] = entity[props] || [];
-                    entity[props].push(key);
-
-                }
-                result = key
-            } else if (props.length >= 1) {
-                props = props.join('');
-                entities[props] = entities[props] || [];
-                entities[props].push(key);
-                result = key;
-            }
-        } else if (a === ACTION_UPDATE) {
-            let [entity, props] = await getNearestEntity(params);
-
-            if (entity) {
-                if (Array.isArray(entity)) {
-                    throw Error('Invalid Path / Id');
-                }
-                result = entity;
-                for (let i = 0; i < props.length; i++) {
-                    result = result[props[i]];
-                }
+async function processRequest(params, query) {
+    const {a, ...queries} = query;
+    let result = undefined;
+    let [entity, props] = await getNearestEntity(params);
+    if (a === ACTION_CREATE) {
+        let key = '';
+        if (entity) {
+            if (Array.isArray(entity)) {
+                key = await createEntity(queries);
+                entity.push(key);
             } else {
+                props = props.join('');
+                key = await createEntity({...queries, parent_: {id_: entity.id_, property_: props}});
+                entity[props] = entity[props] || [];
+                entity[props].push(key);
+            }
+            result = key
+        } else if (props.length >= 1) {
+            key = await createEntity(queries);
+            props = props.join('');
+            rootDb[props] = rootDb[props] || [];
+            rootDb[props].push(key);
+            result = key;
+        }
+    } else if (a === ACTION_UPDATE) {
+        if (entity) {
+            if (Array.isArray(entity)) {
                 throw Error('Invalid Path / Id');
             }
-        } else if (a === ACTION_DELETE) {
-
+            result = entity;
+            Object.keys(queries).forEach(q => {
+                result[q] = queries[q];
+            });
+            result.modified_ = new Date().toISOString();
         } else {
-            const [entity, props] = await getNearestEntity(params);
-            if (entity) {
-                result = entity;
-                for (let i = 0; i < props.length; i++) {
-                    result = result[props[i]];
-                }
+            throw Error('Invalid Path / Id');
+        }
+    } else if (a === ACTION_DELETE) {
+
+        if (entity && props.length === 0) {
+            if (Array.isArray(entity)) {
+                throw Error('Invalid Path / Id');
+            }
+            if (entity.parent_) {
+                const parentEntity = await getEntity(entity.parent_.id_);
+                parentEntity[entity.parent_.property_] = parentEntity[entity.parent_.property_].filter(d => d !== entity.id_);
+                delete rootDb[entity.id_];
+                result = `Deleted`;
+            }
+        } else {
+            throw Error('Invalid Path / Id');
+        }
+    } else {
+        const [entity, props] = await getNearestEntity(params);
+        if (entity) {
+            result = entity;
+            for (let i = 0; i < props.length; i++) {
+                result = result[props[i]];
             }
         }
-        res.json({error: false, params, query, result: result});
-    })();
+    }
+    if (result === undefined) {
+        throw Error('Invalid Path / Id');
+    }
+    return result;
+}
 
+
+router.get('/*', (req, res) => {
+    const {params, query} = getParamsAndQuery(req);
+    processRequest(params, query).then(result => res.json({error: false, result})).catch(err => {
+        console.error(err);
+        res.json({error: err.message});
+    });
+});
+
+router.post('/*', (req, res) => {
+    const {params, query} = getParamsAndQuery(req);
+    processRequest(params, query).then(result => res.json({error: false, result})).catch(err => {
+        console.error(err);
+        res.json({error: err.message});
+    });
 });
 
 function toCamelCase(object) {
