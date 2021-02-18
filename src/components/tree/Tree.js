861e16f277086537767724ec1517aead0751da33
Arif Rachim <marsa.emreef@gmail.com>
2021-02-18 23:15:35 +0400

    Feat : Improve Tree

--- src/components/tree/Tree.js
+++ src/components/tree/Tree.js
@@ -1,16 +1,9 @@
+import {useEffect, useState} from "react";
 import {Horizontal, Vertical} from "components/layout/Layout";
 import List from "components/list/List";
-import useObserver, {useObserverListener} from "components/useObserver";
-
-function DefaultTreeItemRender({data}) {
-    const key = data.key_;
-    const level = key.length;
-    return <Horizontal>
-        <Vertical>{level}</Vertical>
-        <Vertical>{JSON.stringify(data)}</Vertical>
-    </Horizontal>
-}
-
+import useObserver, {ObserverValue, useObserverListener, useObserverValue} from "components/useObserver";
+import Button from "components/button/Button";
+import {v4 as uuid} from "uuid";
 
 const DEFAULT_DATA_KEY = (data) => data?.id;
 
@@ -27,7 +20,7 @@ const DEFAULT_DATA_KEY = (data) => data?.id;
  */
 export default function Tree({
                                  $data,
-                                 itemRenderer = DefaultTreeItemRender,
+                                 itemRenderer = DefaultTreeItemRenderer,
                                  dataKey = DEFAULT_DATA_KEY,
                                  onKeyboardDown,
                                  onKeyboardUp,
@@ -35,15 +28,22 @@ export default function Tree({
                              }) {
 
     const [$listData, setListData] = useObserver(() => flatArray($data.current, [], [], dataKey));
+    const [$collapsedNode, setCollapsedNode] = useObserver({});
 
     useObserverListener($data, (data) => {
-        setListData(flatArray(data, [], [], dataKey));
+        setListData(filterWithCollapsedNode(flatArray(data, [], [], dataKey), $collapsedNode.current));
+    });
+    useObserverListener($collapsedNode, (collapsedNode) => {
+        setListData(filterWithCollapsedNode(flatArray($data.current, [], [], dataKey), collapsedNode));
     })
 
     // lets make the data flat first
-    return <List $data={$listData} itemRenderer={itemRenderer} dataKey={dataKey} onKeyboardUp={onKeyboardUp}
-                 onKeyboardDown={onKeyboardDown} {...props} />
+    return <List $data={$listData} itemRenderer={DefaultTreeItemRenderer} listRenderer={itemRenderer} dataKey={dataKey}
+                 onKeyboardUp={onKeyboardUp}
+                 onKeyboardDown={onKeyboardDown} $collapsedNode={$collapsedNode}
+                 setCollapsedNode={setCollapsedNode} {...props} />
 }
+
 
 const flatArray = (array, result, parentKey, dataKey) => {
     return array.reduce((acc, next) => {
@@ -57,4 +57,128 @@ const flatArray = (array, result, parentKey, dataKey) => {
         }
         return acc;
     }, result);
+}
+
+const filterWithCollapsedNode = (array, collapsedNode) => {
+    return array.filter((data) => {
+        const key = data.key_.join(':');
+        for (const collapsedKey of Object.keys(collapsedNode)) {
+            if (key !== collapsedKey && key.indexOf(collapsedKey) === 0) {
+                return false;
+            }
+        }
+        return true;
+    })
+}
+
+
+/**
+ * This is the function that will loop into the tree and find the object in the tree.
+ * @param {any[]} oldData
+ * @param {string[]} key
+ * @param {function(data):string} dataKey
+ * @returns {any}
+ */
+function findTreeDataFromKey(oldData = [], key = [], dataKey) {
+    const [keyPath, ...rest] = key;
+    const [filteredData] = oldData.filter(data => dataKey(data) === keyPath);
+    if (rest && rest.length > 0) {
+        return findTreeDataFromKey(filteredData.children, rest, dataKey);
+    }
+    return filteredData;
+}
+
+function ToggleButton({$open, setOpen, width}) {
+    const open = useObserverValue($open);
+    return <Button p={0} pT={0} pB={0} vAlign={"center"} color={'light'} opacity={0} b={0} width={width}
+                   onClick={() => setOpen(val => !val)}>
+        {open &&
+        <svg viewBox='0 0 512 512'>
+            <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='48'
+                  d='M112 184l144 144 144-144'/>
+        </svg>
+        }
+        {!open &&
+        <svg viewBox='0 0 512 512'>
+            <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='48'
+                  d='M184 112l144 144-144 144'/>
+        </svg>
+        }
+    </Button>;
+}
+
+export function DefaultTreeItemRenderer({
+                                            $selectedItem,
+                                            setSelectedItem,
+                                            index,
+                                            data,
+                                            setData,
+                                            dataKey,
+                                            listRenderer,
+                                            $collapsedNode,
+                                            setCollapsedNode,
+                                            ...props
+                                        }) {
+
+    const key = data.key_.join(':');
+
+    const [$collapsed, setCollapsed] = useObserver($collapsedNode.current[key] === true);
+    useObserverListener($collapsed, (value) => {
+        if (value) {
+            setCollapsedNode(oldKey => {
+                return {...oldKey, [key]: true};
+            });
+        } else {
+            setCollapsedNode(oldKey => {
+                delete oldKey[key];
+                return {...oldKey};
+            });
+        }
+    })
+    useObserverListener($collapsedNode, () => {
+        setCollapsed($collapsedNode.current[key] === true);
+    });
+
+
+    const level = data.key_.length;
+    const [selected, setSelected] = useState(false);
+    const [$expand, setExpand] = useObserver(!$collapsed.current);
+
+    useObserverListener($expand, (isExpand) => {
+        setCollapsed(!isExpand);
+    });
+
+    const [$toggleButtonVisible, setToggleButtonVisible] = useObserver(data.children > 0);
+    useObserverListener($selectedItem, (selectedItem) => setSelected(selectedItem === data));
+    useEffect(() => setToggleButtonVisible(data.children > 0), [data, setToggleButtonVisible]);
+    const Component = listRenderer;
+    return <Horizontal onClick={() => setSelectedItem(data)} color={"light"} brightness={selected ? -10 : -2}>
+        <Horizontal width={(level - 1) * 15}/>
+        <ObserverValue $observer={$toggleButtonVisible} render={({value}) => {
+            if (!value) {
+                return <Horizontal width={15}/>
+            }
+            return <ToggleButton $open={$expand} setOpen={setExpand} width={15}/>
+        }}/>
+        <Vertical flex={1}>
+            <Component selected={selected}
+                       index={index}
+                       $selectedItem={$selectedItem}
+                       $collapsed={$collapsed}
+                       data={data}
+                       setData={setData}
+                       dataKey={dataKey}
+                       {...props}/>
+        </Vertical>
+        <Button p={0} pL={2} pR={2} onClick={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            setData(oldData => {
+                const originalData = findTreeDataFromKey(oldData, data.key_, dataKey);
+                originalData.children = originalData.children || [];
+                originalData.children.push({id: uuid(), name: 'ABC 123', children: []});
+                return [...oldData];
+            });
+        }}>Add Child</Button>
+    </Horizontal>
 }
--- src/module/sample/tree/index.js
+++ src/module/sample/tree/index.js
@@ -1,9 +1,8 @@
 import {Horizontal, Vertical} from "components/layout/Layout";
 import Tree from "components/tree/Tree";
-import useObserver, {ObserverValue, useObserverListener, useObserverValue} from "components/useObserver";
+import useObserver from "components/useObserver";
 import {v4 as uuid} from "uuid";
 import Button from "components/button/Button";
-import {useEffect, useState} from "react";
 
 export default function TreeSample() {
     const [$data, setData] = useObserver([{
@@ -24,73 +23,14 @@ export default function TreeSample() {
         <Tree $data={$data}
               $selectedItem={$selectedItem}
               setSelectedItem={setSelectedItem}
-              itemRenderer={DataItemRenderer}
+              itemRenderer={MyComponent}
               setData={setData}
         />
     </Vertical>
 }
 
-/**
- * This is the function that will loop into the tree and find the object in the tree.
- * @param {any[]} oldData
- * @param {string[]} key
- * @param {function(data):string} dataKey
- * @returns {any}
- */
-function findTreeDataFromKey(oldData = [], key = [], dataKey) {
-    const [keyPath, ...rest] = key;
-    const [filteredData] = oldData.filter(data => dataKey(data) === keyPath);
-    if (rest && rest.length > 0) {
-        return findTreeDataFromKey(filteredData.children, rest, dataKey);
-    }
-    return filteredData;
-}
-
-function ToggleButton({$open, setOpen, width}) {
-    const open = useObserverValue($open);
-    return <Button p={0} pT={0} pB={0} vAlign={"center"} color={'light'} opacity={0} b={0} width={width}
-                   onClick={() => setOpen(val => !val)}>
-        {open &&
-        <svg viewBox='0 0 512 512'>
-            <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='48'
-                  d='M112 184l144 144 144-144'/>
-        </svg>
-        }
-        {!open &&
-        <svg viewBox='0 0 512 512'>
-            <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='48'
-                  d='M184 112l144 144-144 144'/>
-        </svg>
-        }
-    </Button>;
-}
-
-function DataItemRenderer({$selectedItem, setSelectedItem, index, data, setData, dataKey}) {
-    const level = data.key_.length;
-    const [selected, setSelected] = useState(false);
-    const [$expand, setExpand] = useObserver(true);
-    const [$toggleButtonVisible, setToggleButtonVisible] = useObserver(data.children > 0);
-    useObserverListener($selectedItem, (selectedItem) => setSelected(selectedItem === data));
-    useEffect(() => setToggleButtonVisible(data.children > 0), [data, setToggleButtonVisible]);
-
-    return <Horizontal onClick={() => setSelectedItem(data)} color={"light"} brightness={selected ? -10 : -2}>
-        <Horizontal width={(level - 1) * 15}/>
-        <ObserverValue $observer={$toggleButtonVisible} render={({value}) => {
-            if (!value) {
-                return <Horizontal width={15}/>
-            }
-            return <ToggleButton $open={$expand} setOpen={setExpand} width={15}/>
-        }}/>
-        <Vertical flex={5} pT={0.5}>{data.name}</Vertical>
-        <Button p={0} pL={2} pR={2} onClick={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            setData(oldData => {
-                const originalData = findTreeDataFromKey(oldData, data.key_, dataKey);
-                originalData.children = originalData.children || [];
-                originalData.children.push({id: uuid(), name: 'ABC 123', children: []});
-                return [...oldData];
-            });
-        }}>Add Child</Button>
-    </Horizontal>
+function MyComponent(props) {
+    return <Vertical vAlign={'center'} pT={0.5}>
+        {props.data.name}
+    </Vertical>
 }
