bc5bfc2c88ac20b14bda8e4324dda3ee897e12c7
Arif Rachim <marsa.emreef@gmail.com>
2021-03-16 20:40:43 +0400

    Feat : Update control for date and time

--- src/module/sample/form/index.js
+++ src/module/sample/form/index.js
@@ -1,4 +1,4 @@
-import {Vertical} from "../../../components/layout/Layout";
+import {Horizontal, Vertical} from "../../../components/layout/Layout";
 import useForm, {Controller} from "../../../components/useForm";
 import Input from "../../../components/input/Input";
 import {useObserverListener} from "../../../components/useObserver";
@@ -6,21 +6,39 @@ import InputNumber from "components/input/InputNumber";
 import Button from "components/button/Button";
 import InputDate from "components/input/InputDate";
 import InputTime from "components/input/InputTime";
+import {isNullOrUndefined} from "components/utils";
 
-export default function Form() {
+const requiredValidator = (value) => {
+    if (isNullOrUndefined(value)) {
+        return 'Value required';
+    }
+    return '';
+}
+
+function Form() {
     const {control, handleSubmit, $value} = useForm();
     useObserverListener($value, value => {
         console.log(value);
     })
-    return <Vertical>
+    return <Vertical p={2}>
         <form action="" onSubmit={handleSubmit(data => {
             debugger
         })}>
-            <Controller label={'One'} control={control} render={Input} name={"One"}/>
-            <Controller label={'Two'} control={control} render={InputNumber} name={"Two"}/>
-            <Controller label={'Three'} control={control} render={InputDate} name={"Three"}/>
-            <Controller label={'Four'} control={control} render={InputTime} name={"Four"}/>
-            <Button type={'submit'}>Save</Button>
+            <Vertical gap={2}>
+                <Controller label={'One'} control={control} render={Input} name={"One"} validator={requiredValidator}/>
+                <Controller label={'Two'} control={control} render={InputNumber} name={"Two"}
+                            validator={requiredValidator}/>
+                <Controller label={'Three'} control={control} render={InputDate} name={"Three"}
+                            validator={requiredValidator}/>
+                <Controller label={'Four'} control={control} render={InputTime} name={"Four"}
+                            validator={requiredValidator}/>
+                <Horizontal hAlign={'right'} pT={4}>
+                    <Button type={'submit'} color={"primary"}>Save</Button>
+                </Horizontal>
+            </Vertical>
         </form>
     </Vertical>
 }
+
+Form.title = 'Form'
+export default Form;
