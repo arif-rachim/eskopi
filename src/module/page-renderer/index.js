feeb264506ae02fcc86b37a7cb02a763a6e12072
Arif Rachim <marsa.emreef@gmail.com>
2021-03-20 11:51:37 +0400

    Feat : Add render to save message

--- src/module/page-renderer/index.js
+++ src/module/page-renderer/index.js
@@ -1,16 +1,16 @@
-import {Vertical} from "../../components/layout/Layout";
 import useResource, {useResourceListener} from "components/useResource";
 import {SYSTEM_PAGE_DESIGNS, SYSTEM_PAGES} from "components/SystemTableName";
 import useObserver, {ObserverValue, useObserverMapper} from "components/useObserver";
 import useForm from "components/useForm";
 import {ControllerMapper} from "module/page-renderer/ControllerMapper";
+import {useInfoMessage} from "components/dialog/Dialog";
 
 export default function PageRenderer({params, setTitle}) {
     const [pageId] = params;
     const [$onPageDesignLoad] = useResource({url: `/db/${SYSTEM_PAGE_DESIGNS}`, data: {pageId}});
     const [$onPageLoad] = useResource({url: `/db/${SYSTEM_PAGES}`});
     const [$pageDesign, setPageDesign] = useObserver();
-
+    const [$pageInfo, setPageInfo] = useObserver();
     useResourceListener($onPageDesignLoad, (status, pageDesign) => {
         if (status === 'success') {
             if (pageDesign.length > 0) {
@@ -37,11 +37,24 @@ export default function PageRenderer({params, setTitle}) {
             const detail = pageInfoDetail(pageInfo);
             if (detail) {
                 setTitle(detail.name);
+                setPageInfo(detail);
             }
         }
     })
-    const {control} = useForm();
-    return <Vertical>
+    const {control, handleSubmit, reset} = useForm();
+    const [$onSavePage, doSavaPage] = useResource();
+    const showInfo = useInfoMessage();
+    useResourceListener($onSavePage, (status, response) => {
+        if (status === 'success') {
+            showInfo('Data saved succesfully');
+            reset(response);
+        }
+    });
+
+    return <form action="" style={{height: '100%', display: 'flex', flexDirection: 'column'}}
+                 onSubmit={handleSubmit((data) => {
+                     doSavaPage(`/db/${$pageInfo.current.id}`, {...data, a: 'c'});
+                 })}>
         <ObserverValue $observers={useObserverMapper($pageDesign, data => data?.children)}>{
             (children) => {
                 children = children || [];
@@ -51,7 +64,7 @@ export default function PageRenderer({params, setTitle}) {
                 });
             }
         }</ObserverValue>
-    </Vertical>
+    </form>
 }
 
 
