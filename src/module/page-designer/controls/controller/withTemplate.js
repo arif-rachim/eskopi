123e4cb00df19c4ecfb0a35dc041a875545186e9
arif rachim <marsa.emreef@gmail.com>
2021-03-28 18:44:29 +0400

    Feat : Introduce mechanism to save form

--- src/module/page-designer/controls/controller/withTemplate.js
+++ src/module/page-designer/controls/controller/withTemplate.js
@@ -2,6 +2,7 @@ import {useObserverListener} from "components/useObserver";
 import {isNullOrUndefined} from "components/utils";
 import {handleDragOverControlComponent} from "module/page-designer/designer/handleDragOverControlComponent";
 import {useState} from "react";
+import style from "./WithTemplate.module.css";
 
 export default function withTemplate(Component) {
     return function ControlledComponent({
@@ -12,34 +13,39 @@ export default function withTemplate(Component) {
         const {id} = data;
         const [isHovered, setHovered] = useState(false);
         const [isFocused, setFocused] = useState(false);
-
         const {$selectedController, setSelectedController} = controllerProps;
-
         useObserverListener($selectedController, selectedController => {
             if (isNullOrUndefined(selectedController)) {
                 return setFocused(false);
             }
             setFocused(selectedController.id === id);
         });
-
+        const classNames = [];
+        if(isFocused){
+            classNames.push(style.onFocus);
+        }else if(isHovered){
+            classNames.push(style.onHover);
+        }else{
+            classNames.push(style.onEmpty);
+        }
         return <Component data={data}
                           control={control}
                           containerProps={{
                               onDragOver: handleDragOverControlComponent()
                           }}
+                          className={classNames}
                           style={{
-                              border: isFocused ? '1px solid blue' : isHovered ? '1px solid green' : '1px solid rgba(0,0,0,0.2)',
-                              transition: 'all 100ms cubic-bezier(0,0,0.7,0.9)'
+                              transition: 'all 300ms cubic-bezier(0,0,0.7,0.9)'
                           }}
-                          onMouseEnter={(event) => {
+                          onMouseOver={(event) => {
                               event.stopPropagation();
                               event.preventDefault();
-                              setHovered(true);
+                              setHovered(event.currentTarget.getAttribute('data-id') === id);
                           }}
-                          onMouseLeave={(event) => {
+                          onMouseOut={(event) => {
                               event.stopPropagation();
                               event.preventDefault();
-                              setHovered(false);
+                              setHovered(event.currentTarget.getAttribute('data-id') !== id);
                           }}
                           onClick={(event) => {
                               event.preventDefault();
@@ -48,7 +54,7 @@ export default function withTemplate(Component) {
                                   setSelectedController(data);
                               }
                           }}
-
+                          data-id={id}
                           {...controllerProps}
         />
     }
